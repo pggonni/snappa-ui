3752e4817930a99a95074cccb4646a6c76a3195d
pggonni <59372630+pggonni@users.noreply.github.com>
2020-05-17 08:58:13 -0400

    Refactored and fixed the Extra Players script.
    The old script was buggy and unsightly. About 40 lines of broken code.
    It has been completely refactored to 27 lines and now works every time.
    There is probably a better way to write this code and shrink it even more.

--- dist/html/app.js
+++ dist/html/app.js
@@ -1,81 +1,62 @@
+// ADD AND REMOVE PLAYERS FROM FORM
+// Initialize variables with all the buttons required for the interactivity
 const addPlayerA = document.querySelector('#add-player-a');
 const addPlayerB = document.querySelector('#add-player-b');
 const a3Group = document.querySelector('#a3-form-group');
 const a4Group = document.querySelector('#a4-form-group');
 const b3Group = document.querySelector('#b3-form-group');
 const b4Group = document.querySelector('#b4-form-group');
-
-// Add players in team A
-let counterA = 1;
-addPlayerA.addEventListener('click', function() {
-    if (counterA <= 1){
-        a3Group.classList.remove('hidden');
-        counterA++;
-    } else {
-        a4Group.classList.remove('hidden');
-        counterA--
-        addPlayerA.style.display = 'none';
-        a3Exit.style.display = 'none';
-    }
-});
-
-// Add players in team B
-let counterB = 1;
-addPlayerB.addEventListener('click', function() {
-    if (counterB <= 1){
-        b3Group.classList.remove('hidden');
-        counterB++;
-    } else {
-        b4Group.classList.remove('hidden');
-        counterB--
-        addPlayerB.style.display = 'none';
-        b3Exit.style.display = 'none';
-    }
-});
-
-// // Form Validation attempt
-// const fields = document.querySelectorAll('.form-control');
-// const note = document.querySelector('#note')
-
-// console.log(fields)
-
-// for (let field of fields) {
-//     if (field.value === "") {
-//       note.style.display = 'block';
-//     } else {
-//         note.style.display = 'hidden';
-//     }
-// }
-
 const a3Exit = document.querySelector('#a3-x');
 const a4Exit = document.querySelector('#a4-x');
 const b3Exit = document.querySelector('#b3-x');
-const b4Exit = document.querySelector('#b3-x');
+const b4Exit = document.querySelector('#b4-x');
 
-const closeParent = function(el) {
-    el.parentElement.parentElement.classList.add('hidden');
-}
+/* Initialize a function that accepts:
+        p3: Player 3
+        p3Close: Player 3 Close Button
+        p4: Player 4
+        p4Close: Player 4 Close Button
+        addBtn: The "Add a Player" button
+*/
+const extraPlayers = (p3, p3Close, p4, p4Close, addBtn) => {
 
-a3Exit.addEventListener('click', function(){
-    closeParent(a3Exit);
-    counterA--
-});
+    // Initialize variables to keep track of each additional player's show state
+    let isP3Show = false;
+    let isP4Show = false;
 
-a4Exit.addEventListener('click', function(){
-    closeParent(a4Exit);
-    counterA--
-    addPlayerA.style.display = 'flex';
-    a3Exit.style.display = 'block';
-});
+    // Listen for clicks on the addBtn
+    addBtn.addEventListener('click', () => {
+        // If p3 is hidden: show p3 and set its show state to true
+        if (isP3Show === false) {
+            p3.classList.remove('hidden');
+            isP3Show = true;
 
-b3Exit.addEventListener('click', function(){
-    closeParent(b3Exit);
-    counterB--
-});
+        // If p3 is already showing: show p4, hide the addBtn, and hide the p3 close button
+        } else {
+            isP4Show = true;
+            p4.classList.remove('hidden');
+            addBtn.classList.add('hidden');
+            p3Close.classList.add('hidden');
+        };
+    });
 
-b4Exit.addEventListener('click', function(){
-    closeParent(b4Exit);
-    counterB--
-    addPlayerB.style.display = 'flex';
-    b3Exit.style.display = 'block';
-});
+    // Listen for a click on p3Close
+    p3Close.addEventListener('click', () => {
+        // Hide p3 and set its show state to false
+        p3.classList.add('hidden');
+        isP3Show = false; 
+    });
+
+    // Listen for a click on p4Close
+    p4Close.addEventListener('click', () => {
+        // Hide p4, set its show state to false, show the addBtn, and show p3Close
+        p4.classList.add('hidden');
+        isP4Show = false; 
+        addBtn.classList.remove('hidden');
+        p3Close.classList.remove('hidden');
+     });
+};
+
+// Use the function on both teams
+extraPlayers(a3Group, a3Exit, a4Group, a4Exit, addPlayerA);
+extraPlayers(b3Group, b3Exit, b4Group, b4Exit, addPlayerB);
